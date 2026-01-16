d588d6af56cabdb722505956d53055636d277936
Dimas Wahyu Nugraha <dimaswahyu12537@gmail.com>
2026-01-16 17:11:41 +0700

    comit

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-const reveals = document.querySelectorAll(".reveal");
+const reveals = document.querySelectorAll('.reveal');
 
-function revealOnScroll() {
+window.addEventListener('scroll', () => {
   reveals.forEach(el => {
     const top = el.getBoundingClientRect().top;
     if (top < window.innerHeight - 100) {
-      el.classList.add("active");
+      el.classList.add('active');
     }
   });
-}
-
-window.addEventListener("scroll", revealOnScroll);
-revealOnScroll();
+});
